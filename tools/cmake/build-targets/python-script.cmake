eb28485613b15afa486f5966bc673f11f952cef1
no-country-4-old-code <NoCountry4OldCode@gmail.com>
2025-04-01 10:44:34 +0000

    Adapt pytthon-script.cmake to receive argument of python-script-path

--- tools/cmake/build-targets/python-script.cmake
+++ tools/cmake/build-targets/python-script.cmake
@@ -1,11 +1,10 @@
 ##
-# Creates a custom target to run a python script.
+# Creates a custom target to run python script _SCRIPT.
 ##
-function(create_target_python_script)
-    # Use function to not pollute shared namespace with custom variables
-    set(_TOOL_TARGET_NAME "python-script")
-    set(_TOOL_OUTPUT_PATH "${PROJECT_REPORT_PATH}/metrics")
-    set(_TOOL_OUTPUT_FILENAME "${_TOOL_OUTPUT_PATH}/Stats.txt")
+function(create_target_python_script _SCRIPT_PATH)
+    get_filename_component(_SCRIPT_NAME ${_SCRIPT_PATH} NAME)
+    set(_TOOL_TARGET_NAME "script-${_SCRIPT_NAME}")
+    set(_TOOL_OUTPUT_PATH "${PROJECT_REPORT_PATH}/scripts")
 
     find_package(Python3 3.8 REQUIRED)
 
@@ -19,17 +18,15 @@ function(create_target_python_script)
 
     if(NOT TARGET ${_TOOL_TARGET_NAME})
 
-        set(_SCRIPT_NAME "detect-dependencies.py")
-
         add_custom_target(
             ${_TOOL_TARGET_NAME}
             # Ensure the output directory is created
             COMMAND ${CMAKE_COMMAND} -E make_directory "${_TOOL_OUTPUT_PATH}"
             # Run python script
-            COMMAND ${Python3_EXECUTABLE} ${_SCRIPT_NAME} ${SRC_FOLDER_PATH}
+            COMMAND ${Python3_EXECUTABLE} "${_SCRIPT_NAME}" ${SRC_FOLDER_PATH}
             WORKING_DIRECTORY ${TOOLS_FOLDER_PATH}/scripts
             COMMENT "Runs python script: ${_SCRIPT_NAME}")
     endif()
 endfunction()
 
-create_target_python_script()
+create_target_python_script(${SCRIPT_FOLDER_PATH}/example.py)
---
+++ tools/scripts/example.py
@@ -0,0 +1,7 @@
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python dependency_analyzer.py <directory>")
+        sys.exit(1)
+    print("Example script")
