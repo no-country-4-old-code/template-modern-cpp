eb3c04490885f3ca458eb7fa50e7b46411a2ce9d
no-country-4-old-code <NoCountry4OldCode@gmail.com>
2025-04-01 10:58:49 +0000

    Move SCRIPT_FOLDER_PATH from root CMakeLists to python-script.cmake

--- tools/cmake/build-targets/python-script.cmake
+++ tools/cmake/build-targets/python-script.cmake
@@ -1,4 +1,6 @@
 include(create-python-script-target)
 
+set(SCRIPT_FOLDER_PATH ${TOOLS_FOLDER_PATH}/scripts)
+
 create_target_python_script(${SCRIPT_FOLDER_PATH}/example.py)
 create_target_python_script(${SCRIPT_FOLDER_PATH}/detect-dependencies.py)
