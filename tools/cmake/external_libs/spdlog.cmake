d22940f3ddbbb2bb42d85bf754e1d59ed3025af5
no-country-4-old-code <NoCountry4OldCode@gmail.com>
2025-04-01 13:33:52 +0000

    Remove spdlog.cmake

--- tools/cmake/external_libs/spdlog.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    spdlog
-    GIT_REPOSITORY "https://github.com/gabime/spdlog"
-    GIT_TAG v1.15.1
-    GIT_SHALLOW TRUE)
-FetchContent_MakeAvailable(spdlog)
