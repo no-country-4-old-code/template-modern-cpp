d5eee081de5e49a7aea62159b791aa65fb315a41
no-country-4-old-code <NoCountry4OldCode@gmail.com>
2025-04-01 11:47:29 +0000

    Remove cycle-dependency from detect-dependency script. Something is not working correctly - I will investigate later on. Until then the feature is removed.

--- tools/scripts/detect-dependencies.py
+++ tools/scripts/detect-dependencies.py
@@ -14,6 +14,7 @@ def parse_includes(file_path):
                 includes.add(match.group(1))
     return includes
 
+
 def build_dependency_graph(files):
     """Construct a dependency graph from the source files."""
     graph = defaultdict(set)
@@ -27,29 +28,6 @@ def build_dependency_graph(files):
                 reverse_graph[include].add(filename)
     return graph, reverse_graph
 
-def detect_cycles(graph):
-    """Detect cycles in the dependency graph using DFS."""
-    def visit(node, visited, stack):
-        if node in stack:
-            return True, stack[stack.index(node):]  # Cycle detected
-        if node in visited:
-            return False, []
-        
-        visited.add(node)
-        stack.append(node)
-        for neighbor in graph.get(node, []):
-            has_cycle, cycle_path = visit(neighbor, visited, stack)
-            if has_cycle:
-                return True, cycle_path
-        stack.pop()
-        return False, []
-    
-    visited = set()
-    for node in graph:
-        has_cycle, cycle_path = visit(node, visited, [])
-        if has_cycle:
-            return True, cycle_path
-    return False, []
 
 def analyze_dependencies(files):
     graph, reverse_graph = build_dependency_graph(files)
@@ -66,16 +44,10 @@ def analyze_dependencies(files):
         incoming = len(reverse_graph[file])
         outgoing = len(graph[file])
         print(f"{file:<12} | (->) {incoming:^7} | (<-) {outgoing:^7}")
+    print("\n")
     
-    has_cycle, cycle_path = detect_cycles(graph)
-    if has_cycle:
-        print("\n===Cyclic Dependency Detected:")
-        print(" -> ".join(cycle_path) + " -> " + cycle_path[0])
-    else:
-        print("\n=== No cyclic dependencies detected.")
 
 if __name__ == "__main__":
     directory = utils.get_input_arguments()
     files = utils.find_source_files(directory)
-
     analyze_dependencies(files)
