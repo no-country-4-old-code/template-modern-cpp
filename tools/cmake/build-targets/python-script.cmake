a9aea2a85fc28b6974af7aa300436147dbe8c612
no-country-4-old-code <NoCountry4OldCode@gmail.com>
2025-04-01 12:18:37 +0000

    Rename example.py to find-todos.py

--- tools/cmake/build-targets/python-script.cmake
+++ tools/cmake/build-targets/python-script.cmake
@@ -2,5 +2,5 @@ include(create-python-script-target)
 
 set(SCRIPT_FOLDER_PATH ${TOOLS_FOLDER_PATH}/scripts)
 
-create_target_python_script(${SCRIPT_FOLDER_PATH}/example.py)
+create_target_python_script(${SCRIPT_FOLDER_PATH}/find-todos.py)
 create_target_python_script(${SCRIPT_FOLDER_PATH}/detect-dependencies.py)
