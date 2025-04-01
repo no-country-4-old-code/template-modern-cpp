6bf3565c3edfae90b467614a916fd53d7b9e336f
no-country-4-old-code <NoCountry4OldCode@gmail.com>
2025-04-01 11:36:51 +0000

    refactor utils.py

--- tools/scripts/utils/utils.py
+++ tools/scripts/utils/utils.py
@@ -5,7 +5,7 @@ def get_input_arguments():
     number_of_arguments = len(sys.argv) - 1;
 
     if number_of_arguments != 1:
-        print("ERROR: Wrong number of arguments ! (Expected 1 ; Got {})".format(number_of_arguments))
+        print("ERROR: Wrong number of arguments ! (Expected 1 ; Got {number_of_arguments})")
         print("Usage: python <script-name.py> <directory>")
         sys.exit(1)
     
@@ -15,11 +15,12 @@ def get_input_arguments():
 
 
 def find_source_files(directory):
-    """Find all C/C++ source and header files in the given directory."""
     extensions = {'.h', '.hpp', '.c', '.cpp', '.cc'}
     files = {}
+
     for root, _, filenames in os.walk(directory):
         for filename in filenames:
             if any(filename.endswith(ext) for ext in extensions):
                 files[filename] = os.path.join(root, filename)
+    
     return files
